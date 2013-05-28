ad291f1d5991a4422a5720afbd2cb6b886048a9c
tyrone Gaylord <tyorne.mdd@gmail.com>
2013-05-27 22:33:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreakfastClub::Application.config.secret_token = 'fd90f2a454fcf80882f8837ec8c840787db2542927b8b9ebe343f0d2bf536e75f1b2ed8ba143cbace3c988e03f97162e438e06e197ad8a6540a33fadfaad554a'
