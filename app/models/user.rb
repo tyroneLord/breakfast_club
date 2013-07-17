62ddebd10b917eb6a26ed0d5065799b19e775b2c
Tyrone Gaylord <tyronegaylord@Digitals-MacBook-Pro.local>
2013-07-17 17:00:38 -0500

    added user model and controller and also add roo gem

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+  attr_accessible :breakfast_day, :fname, :lname
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BreakfastClub::Application.routes.draw do
+  get "users/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
